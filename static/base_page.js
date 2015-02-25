b9745c9ae84afd0e4aa00776bc16d978dfe94f38
pcrane <pcrane@goscoutgo.com>
2015-02-25 16:31:01 -0500

    Large-ish update to item layout
    Item displays all fields in layout.
    
    Added delete functionality per-item.
    
    Add item adds to items list.
    
    Validation skeleton function added.

--- static/base_page.js
+++ static/base_page.js
@@ -12,58 +12,31 @@ function serialize_object(form_selector) {
 function validate_item(json) {
     return _.every(_.values(json));
 }
-var items = [];
 
 $(function() {
-    /*
-    var item_list_template = _.template(
-        $("script#item_list_template").html()
-    );
-    var list_params = {
-        items: items
-    };
-    $("div#container").append(item_list_template(list_params));
-
-    $(document).on('click', 'button#add_item_submit', function (e) {
-        var obj = serialize_object("form#item_add_form");
-        var validated = validate_item(obj);
-        console.log(obj);
-        console.log(validated);
-        if(validated) {
-            items.push(obj);
-
-        }
-        return false;
-    });
-    // */
-
-
     var Item = Backbone.Model.extend({
         idAttribute: '_id',
         urlRoot: "",
-        /*
-        schema: {
-            item_id: 'Text'
-        //}/*/
         schema: {
             item_id: 'Text',
-            buy: {
-                buyout: 'Text',
-                amount: 'Text',
-                additive: 'Text'
-            },
-            sell: {
-                min_buyout: 'Text',
-                max_buyout: 'Text',
-                run_time: 'Text',
-                stack_size: 'Text',
-                ignore_stacks_below: 'Text',
-                amount: 'Text',
-                bid_percent: 'Text',
-                undercut_percent: 'Text'
-            }
+            buy_buyout: 'Text',
+            buy_amount: 'Text',
+            buy_additive: 'Text',
+
+            sell_min_buyout: 'Text',
+            sell_max_buyout: 'Text',
+            sell_run_time: 'Text',
+            sell_stack_size: 'Text',
+            sell_ignore_stacks_below: 'Text',
+            sell_amount: 'Text',
+            sell_bid_percent: 'Text',
+            sell_undercut_percent: 'Text'
+        },
+        validate: function(attrs, options) {
+            //console.log(attrs);
+            //console.log("validating b.addi: "+attrs.buy_additive);
+            //console.log("validating b.addi: "+attrs.buy_additive);
         }
-        //*/
     });
     var Items = Backbone.Collection.extend({
         model: Item,
@@ -75,55 +48,88 @@ $(function() {
         },
         template: "#item_info_template",
         tagName: "div",
-        className: 'item_info',
         events: {
-            'click span#item_remove': 'removeItem'
+            'click button#btn_delete': 'delete_item',
         },
-        removeItem: function() {
-            console.log('removing item w/id:'+ this.model.get('item_id'));
+        delete_item: function(e) {
             this.model.destroy();
         }
     });
-    var ItemsView = Backbone.Marionette.CollectionView.extend({
-        initialize: function() {
-        },
+    var ItemsView = Backbone.Marionette.CompositeView.extend({
         tagName: "div",
+        id: 'item_accordion',
+        className: 'panel-group wat',
+        attributes: {
+            'role': 'tablist',
+            'aria-multiselectable': 'true'
+        },
         childView: ItemInfoView,
-        template: "#item_data_template"
+        template: "#item_data_template",
+    });
+    var CreateItemView = Backbone.Marionette.ItemView.extend({
+        initialize: function(starting_items) {},
+        template: "#item_add_template",
+        tagName: "div",
+        events: {
+            'click button#add_item_submit': 'add_item'
+        },
+        validate_item: function(item) {
+
+            return true;
+        },
+        add_item: function(e) {
+            var form_data = serialize_object('form#item_add_form');
+
+            if(form_data.buy_additive === 'on') {
+                form_data.buy_additive = "True";
+            } else {
+                form_data.buy_additive = "False";
+            }
+
+            var cur_item = new Item(form_data);
+            cur_item.isValid();
+            if(this.validate_item(cur_item)) {
+                items.push(cur_item);
+            }
+            e.preventDefault();
+        }
     });
 
+    var items = new Items([{
+        item_id: 'item_id',
+        buy_buyout: 'buy_buyout',
+        buy_amount: 'buy_amount',
+        buy_additive: 'True',
 
-    var items = new Items([
-        {
-            item_id: 'abc123',
-            buy: {
-                buyout: '1g1s1c'
-            }
-        },
-        {
-            item_id: 'test',
-            buy: {
-                buyout: '1g1s1c'
-            }
-        }
-    ]);
+        sell_min_buyout: 'sell_min_buyout',
+        sell_max_buyout: 'sell_max_buyout',
+        sell_run_time: 'sell_run_time',
+        sell_stack_size: 'sell_stack_size',
+        sell_ignore_stacks_below: 'sell_ignore_stacks_below',
+        sell_amount: 'sell_amount',
+        sell_bid_percent: 'sell_bid_percent',
+        sell_undercut_percent: 'sell_undercut_percent'
+    }]);
     var MyApp = new Backbone.Marionette.Application();
     MyApp.addRegions({
-      mainRegion: "#content"
+      item_list: "#item_list_anchor",
+      item_add_form: '#item_add_form_anchor'
     });
 
     MyApp.addInitializer(function(options){
       var itemsView = new ItemsView({
         collection: options.items
       });
-      MyApp.mainRegion.show(itemsView);
+      MyApp.item_list.show(itemsView);
+
+      var createView = new CreateItemView();
+      MyApp.item_add_form.show(createView);
     });
 
     MyApp.start({items: items});
 });
 
 /*
-
 var base_template_info;
     $.ajax({
         url: 'static/base_template.js',
